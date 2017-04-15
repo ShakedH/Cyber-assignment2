4b2a8f9340da73c6e7ad4027e3a326443b697128
שקד חזון <hazons@post.bgu.ac.il>
2017-04-15 13:49:19 +0300

    Added CLI flags

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+/**
+ * Created by user on 13/04/2017.
+ */
+
+import org.apache.commons.cli.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // CLI Setup
+        Options options = SetCLIOptions();
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
+        CommandLine cmd;
+        try
+        {
+            cmd = parser.parse(options, args);
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            helpFormatter.printHelp("Cyber",options);
+            System.exit(1);
+            return;
+        }
+    }
+    
+    private static Options SetCLIOptions()
+    {
+        Options options = new Options();
+        
+        Option algorithm = new Option("a", "algorithm", true, "Encryption algorithm");
+        algorithm.setArgs(1);
+        algorithm.setRequired(true);
+        options.addOption(algorithm);
+        
+        Option command = new Option("c", "command", true, "Command to preform");
+        command.setArgs(1);
+        command.setRequired(true);
+        options.addOption(command);
+        
+        Option text = new Option("t", "text", true, "Text File Path");
+        text.setArgs(1);
+        text.setRequired(true);
+        options.addOption(text);
+        
+        Option key = new Option("k", "key", true, "Key File Path");
+        key.setArgs(1);
+        key.setRequired(false);
+        options.addOption(key);
+        
+        Option vector = new Option("v", "vector", true, "Initial Vector File Path");
+        vector.setArgs(1);
+        vector.setRequired(false);
+        options.addOption(vector);
+        
+        Option output = new Option("o", "output", true, "Output File Path");
+        output.setArgs(1);
+        output.setRequired(false);
+        options.addOption(output);
+        
+        Option knownPlainText = new Option("kp", "KnownPlainText", true, "Known Plain Text File Path");
+        knownPlainText.setArgs(1);
+        knownPlainText.setRequired(false);
+        options.addOption(knownPlainText);
+        
+        Option KnownCipherText = new Option("kc", "KnownCipherText", true, "Known Cipher Text File Path");
+        KnownCipherText.setArgs(1);
+        KnownCipherText.setRequired(false);
+        options.addOption(KnownCipherText);
+        
+        return options;
+    }
+}
