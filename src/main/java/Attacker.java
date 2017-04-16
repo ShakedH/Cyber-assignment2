7da7718f99ad9f19360d0fba4345e316b4d8179d
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-04-16 12:47:01 +0300

    Attacker class added

--- src/main/java/Attacker.java
+++ src/main/java/Attacker.java
@@ -3,4 +3,22 @@
  */
 public class Attacker
 {
+    private int m_Algorithm;
+    private String m_CipherText;
+    private String m_IV;
+    private String m_OutputPath;
+    
+    public Attacker(String algorithm, String cipherTextPath, String ivPath, String outputPath)
+    {
+        try
+        {
+            m_Algorithm = algorithm.endsWith("10") ? 10 : 52;
+            
+        }
+        catch (Exception ex)
+        {
+        
+        }
+        
+    }
 }
