fc003702abef90e9da0f3527753a9c87d82803c8
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-04-16 15:03:57 +0300

    About time

--- src/main/java/Attacker.java
+++ src/main/java/Attacker.java
@@ -20,7 +20,34 @@ public class Attacker
     
     public void Attack10()
     {
+        char[] permutation = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        String decryptedCipher = BruteForceDecryption(permutation, 0, permutation.length - 1);
+    }
     
+    private String BruteForceDecryption(char[] permutation, int startIndex, int endIndex)
+    {
+        if (startIndex == endIndex)
+        {
+            String segment = m_CipherText.substring(0, 10);
+            
+        }
+        else
+        {
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                swap(permutation, startIndex, i);
+                BruteForceDecryption(permutation, startIndex + 1, endIndex);
+                swap(permutation, startIndex, i);
+            }
+        }
+        return null;
+    }
+    
+    private void swap(char[] array, int a, int b)
+    {
+        char temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
     
     public void Attack52()
