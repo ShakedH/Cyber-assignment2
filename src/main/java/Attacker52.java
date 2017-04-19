07a630d16f6ab17934794be646f4007d8ecb687b
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-04-19 18:07:15 +0300

    KPA

--- src/main/java/Attacker52.java
+++ src/main/java/Attacker52.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Ron Michaeli on 19-Apr-17.
  */
@@ -8,6 +11,7 @@ public class Attacker52
     private byte[] m_KnownCipherText;
     private byte[] m_IV;
     private String m_OutputPath;
+    private Map<Character, Character> m_Key;
     
     public Attacker52(String cipherTextPath, String knownPlainTextPath, String knownCipherTextPath, String ivPath, String outputPath) throws Exception
     {
@@ -16,13 +20,18 @@ public class Attacker52
         m_KnownCipherText = CommonFunctions.ReadBytesFromFile(knownCipherTextPath);
         m_IV = CommonFunctions.ReadBytesFromFile(ivPath);
         m_OutputPath = outputPath;
+        m_Key = new HashMap<Character, Character>();
     }
     
     public void Attack()
     {
         for (int i = 0; i < m_KnownPlainText.length; i++)
         {
-        
+            char xorResult = (char)CommonFunctions.XorByte(m_KnownPlainText[i], m_IV[i]);
+            if (!Character.isLetter(xorResult))
+                continue;
+            if (!m_Key.containsKey(xorResult))
+                m_Key.put(xorResult, (char)m_KnownCipherText[i]);
         }
     }
 }
